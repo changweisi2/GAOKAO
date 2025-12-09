b621ffc93de598f2fd3c4433978b0ecad02fc87a
CADICAT <2453873938@qq.com>
2025-12-09 20:00:20 +0800

    GAOKAO -> classify problems to Fields

--- field_classificition.py
+++ field_classificition.py
@@ -8,4 +8,4 @@ class Field(Enum):
     Sociocultural_Understanding = "sociocultural_understanding"
     Data_and_StatisticalLiteracy = "data&statistical_literacy"
     Commonsense_and_WorldKnowledge = "commonsense_and_world_knowledge"
-    Creative_and_Openended_Questions = "creative_and_openended_questions"
+    Creative_and_Open-ended_Questions = "creative_and_open-ended_questions"
